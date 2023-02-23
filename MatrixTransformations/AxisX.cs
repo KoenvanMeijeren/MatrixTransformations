5d32e715094a4b8c21fad6d0b82d52765dbe26ea
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-02-23 13:57:56 +0100

    Store data in matrix class instead of vertices list

--- MatrixTransformations/AxisX.cs
+++ MatrixTransformations/AxisX.cs
@@ -5,20 +5,20 @@ namespace MatrixTransformations;
 public class AxisX
 {
     private int _size;
-    public readonly List<VectorImmutable> VertexBuffer;
+    public MatrixImmutable Matrix { get; set; }
 
     public AxisX(int size = 100)
     {
         _size = size;
-        VertexBuffer = new List<VectorImmutable>
-        {
+        Matrix = new MatrixImmutable(
             new(0, 0),
             new(size, 0)
-        };
+        );
     }
 
-    public static void Draw(GraphicsHelper graphicsHelper, List<VectorImmutable> vectors)
+    public static void Draw(GraphicsHelper graphicsHelper, MatrixImmutable matrix)
     {
+        var vectors = matrix.Vectors;
         var pen = new Pen(Color.Red, 2f);
         graphicsHelper.DrawLine(pen, vectors[0].X, vectors[0].Y, vectors[1].X, vectors[1].Y);
         var font = new Font("Arial", 10);
--- MatrixTransformations/AxisY.cs
+++ MatrixTransformations/AxisY.cs
@@ -5,20 +5,20 @@ namespace MatrixTransformations;
 public class AxisY
 {
     private int _size;
-    public readonly List<VectorImmutable> VertexBuffer;
+    public MatrixImmutable Matrix { get; set; }
 
     public AxisY(int size = 100)
     {
         _size = size;
-        VertexBuffer = new List<VectorImmutable>
-        {
+        Matrix = new MatrixImmutable(
             new(0, 0),
             new(0, size)
-        };
+        );
     }
 
-    public static void Draw(GraphicsHelper graphicsHelper, List<VectorImmutable> vectors)
+    public static void Draw(GraphicsHelper graphicsHelper, MatrixImmutable matrix)
     {
+        var vectors = matrix.Vectors;
         var pen = new Pen(Color.Green, 2f);
         graphicsHelper.DrawLine(pen, vectors[0].X, vectors[0].Y, vectors[1].X, vectors[1].Y);
         var font = new Font("Arial", 10);
